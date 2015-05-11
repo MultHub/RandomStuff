1e14bc3769b876789fedddaf23ce460ec5bd53b9
Multi <MultHub@users.noreply.github.com>
2015-05-11 12:25:08 +0200

    boat v0.2 yuy
    finally api

--- boat.lua
+++ boat.lua
@@ -1,6 +1,6 @@
 local tArgs = {...}
 
-print("boat v0.1")
+print("boat v0.2")
 local function printUsage()
 	print("Usage: boat <hostname> [pluginDir]")
 end
@@ -94,6 +94,122 @@ if not fs.exists(sPluginPath) then
 	fs.makeDir(sPluginPath)
 end
 
+local events = {
+	connect = {},
+	disconnect = {},
+	chat = {},
+}
+local configDefaults = {
+	connectMessage = "* %s has joined the chat",
+	disconnectMessage = "* %s has left the chat",
+	unknownCommandMessage = "* Unrecognised command: /%s",
+	chatMessage = "<%s> %s",
+}
+local config = {}
+serverEnv = {}
+local reloadPlugins
+local tCommands
+tCommands = {
+	["me"] = function( tUser, sContent )
+		if string.len(sContent) > 0 then
+			send( "* "..tUser.sUsername.." "..sContent )
+		else
+			send( "* Usage: /me [words]", tUser.nUserID )
+		end
+	end,
+	["nick"] = function( tUser, sContent )
+		if string.len(sContent) > 0 then
+			local sOldName = tUser.sUsername
+			tUser.sUsername = sContent
+			send( "* "..sOldName.." is now known as "..tUser.sUsername )
+		else
+			send( "* Usage: /nick [nickname]", tUser.nUserID )
+		end
+	end,
+	["list"] = function( tUser, sContent )
+		send( "* Connected Users:", tUser.nUserID )
+		local sUsers = "*"
+		for nUserID, tUser in pairs( tUsers ) do
+			sUsers = sUsers .. " " .. tUser.sUsername
+		end
+		send( sUsers, tUser.nUserID )
+	end,
+	["help"] = function( tUser, sContent )
+		send( "* Available commands:", tUser.nUserID )
+		local sCommands = "*"
+		for sCommand, fnCommand in pairs( tCommands ) do
+			sCommands = sCommands .. " /" .. sCommand
+		end
+		send( sCommands.." /logout", tUser.nUserID )
+	end,
+	["plugins"] = function(tUser)
+		local tPlugins = fs.list(sPluginPath)
+		send("* Plugins ("..#tPlugins.."): "..table.concat(tPlugins, ", "), tUser.nUserID)
+	end,
+	["reload"] = function(tUser)
+		reloadPlugins()
+		send("* "..tUser.sUsername.." has reloaded the server")
+		send("* Reload complete.", tUser.nUserID)
+	end,
+	["events"] = function(tUser)
+		send("* Registered events:", tUser.nUserID)
+		for eventName, eventList in pairs(events) do
+			send("  - "..eventName..":", tUser.nUserID)
+			for name in pairs(eventList) do
+				send("    - "..name, tUser.nUserID)
+			end
+		end
+	end,
+}
+function serverEnv.setCommand(name, func)
+	if name == "help" then return end
+	tCommands[name] = func
+end
+function serverEnv.getCommand(name)
+	return tCommands[name]
+end
+function serverEnv.setEvent(event, name, func)
+	events[event][name] = func
+end
+function serverEnv.getEvent(event, name)
+	return events[event][name]
+end
+function serverEnv.setConfig(key, value)
+	config[key] = value
+end
+function serverEnv.getConfig(key)
+	return config[key]
+end
+setmetatable(serverEnv, {__index=getfenv()})
+reloadPlugins = function()
+	for i, v in pairs(events) do
+		events[i] = {}
+	end
+	for i, v in pairs(configDefaults) do
+		config[i] = v
+	end
+	for i, v in pairs(fs.list(sPluginPath)) do
+		if fs.isDir(fs.combine(sPluginPath, v)) then
+			-- found a valid plugin
+			if fs.exists(fs.combine(sPluginPath, v) .. "/main.lua") then
+				local fn, fnErr = loadfile(fs.combine(sPluginPath, v) .. "/main.lua", "plugin \"" .. v .. "\"")
+				if not fn then
+					printError("Plugin \""..v.."\" crashed!")
+					printError(fnErr)
+				else
+					local pluginEnv = {
+						getPath = function()
+							return fs.combine(sPluginPath, v)
+						end,
+					}
+					setmetatable(pluginEnv, {__index = serverEnv})
+					pcall(setfenv(fn, pluginEnv))
+				end
+			end
+		end
+	end
+end
+reloadPlugins()
 -- Handle messages
 local ok, err = pcall( function()
 	parallel.waitForAny( function()
@@ -105,6 +221,9 @@ local ok, err = pcall( function()
 				if tUser then
 					if not tUser.bPingPonged then
 						send( "* "..tUser.sUsername.." has timed out" )
+						for i, v in pairs(events.disconnect) do
+							v(tUsers[nUserID])
+						end
 						tUsers[ nUserID ] = nil
 						nUsers = nUsers - 1
 						printUsers()
@@ -117,69 +236,6 @@ local ok, err = pcall( function()
 	end,
 	function()
 		while true do
-			local serverEnv = {}
-			local tCommands
-			tCommands = {
-				["me"] = function( tUser, sContent )
-					if string.len(sContent) > 0 then
-						send( "* "..tUser.sUsername.." "..sContent )
-					else
-						send( "* Usage: /me [words]", tUser.nUserID )
-					end
-				end,
-				["nick"] = function( tUser, sContent )
-					if string.len(sContent) > 0 then
-						local sOldName = tUser.sUsername
-						tUser.sUsername = sContent
-						send( "* "..sOldName.." is now known as "..tUser.sUsername )
-					else
-						send( "* Usage: /nick [nickname]", tUser.nUserID )
-					end
-				end,
-				["list"] = function( tUser, sContent )
-					send( "* Connected Users:", tUser.nUserID )
-					local sUsers = "*"
-					for nUserID, tUser in pairs( tUsers ) do
-						sUsers = sUsers .. " " .. tUser.sUsername
-					end
-					send( sUsers, tUser.nUserID )
-				end,
-				["help"] = function( tUser, sContent )
-					send( "* Available commands:", tUser.nUserID )
-					local sCommands = "*"
-					for sCommand, fnCommand in pairs( tCommands ) do
-						sCommands = sCommands .. " /" .. sCommand
-					end
-					send( sCommands.." /logout", tUser.nUserID )
-				end,
-				["plugins"] = function(tUser)
-					local tPlugins = fs.list(sPluginPath)
-					send("* Plugins ("..#tPlugins.."): "..table.concat(tPlugins, ", "), tUser.nUserID)
-				end,
-			}
-			function serverEnv.setCommand(name, func)
-				if name == "help" then return end
-				tCommands[name] = func
-			end
-			function serverEnv.getCommand(name)
-				return tCommands[name]
-			end
-			setmetatable(serverEnv, {__index=getfenv()})
-			for i, v in pairs(fs.list(sPluginPath)) do
-				if fs.isDir(fs.combine(sPluginPath, v)) then
-					-- found a valid plugin
-					if fs.exists(fs.combine(sPluginPath, v) .. "/main.lua") then
-						local fn, fnErr = loadfile(fs.combine(sPluginPath, v) .. "/main.lua", "plugin \"" .. v .. "\"")
-						if not fn then
-							printError("Plugin \""..v.."\" crashed!")
-							printError(fnErr)
-						else
-							pcall(setfenv(fn, serverEnv))
-						end
-					end
-				end
-			end
-
 			local nSenderID, tMessage = rednet.receive( "chat" )
 			if type( tMessage ) == "table" then
 				if tMessage.sType == "login" then
@@ -194,17 +250,22 @@ local ok, err = pcall( function()
 						}
 						nUsers = nUsers + 1
 						printUsers()
-						send( "* "..sUsername.." has joined the chat" )
+						send( config.connectMessage:format(sUsername) )
+						for i, v in pairs(events.connect) do
+							v(tUsers[nUserID])
+						end
 						ping( nUserID )
 					end
-
 				else
 					-- Something else from existing client
 					local nUserID = tMessage.nUserID
 					local tUser = tUsers[ nUserID ]
 					if tUser and tUser.nID == nSenderID then
 						if tMessage.sType == "logout" then
-							send( "* "..tUser.sUsername.." has left the chat" )
+							send( config.disconnectMessage:format(tUser.sUsername) )
+							for i, v in pairs(events.disconnect) do
+								v(tUsers[nUserID])
+							end
 							tUsers[ nUserID ] = nil
 							nUsers = nUsers - 1
 							printUsers()
@@ -218,22 +279,19 @@ local ok, err = pcall( function()
 										local sContent = string.sub( sMessage, string.len(sCommand)+3 )
 										fnCommand( tUser, sContent )
 									else
-										send( "* Unrecognised command: /"..sCommand, tUser.nUserID )
+										send( config.unknownCommandMessage:format(sCommand), tUser.nUserID )
 									end
 								else
-									send( "<"..tUser.sUsername.."> "..tMessage.sText )
+									send( config.chatMessage:format(tUser.sUsername, sMessage) )
 								end
 							end
-
 						elseif tMessage.sType == "ping to server" then
 							rednet.send( tUser.nID, {
 								sType = "pong to client",
 								nUserID = nUserID,
 							}, "chat" )
-
 						elseif tMessage.sType == "pong to server" then
 							tUser.bPingPonged = true
-
 						end
 					end
 				end
